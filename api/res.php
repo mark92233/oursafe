8ae9f4c71a6f5f810be724fc2677b2be54e88970
mark92233 <154310030+mark92233@users.noreply.github.com>
2026-04-15 08:43:24 +0800

    debug

--- api/res.php
+++ api/res.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/db_related/db_connect.php';
 
 // Handle deletion if the form was submitted
